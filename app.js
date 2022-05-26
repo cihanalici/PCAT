9ed1853f61904e59103df8c42d952f5f134a73c3
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-05-26 11:56:31 +0300

    Method Override

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -19,11 +22,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -31,12 +37,29 @@ app.get('/', async (req, res) => {
 
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
- // res.render('photo');
+  // res.render('photo');
 
- const photo = await Photo.findById(req.params.id)
- res.render('photo', {
-   photo
- })
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`)
 });
 
 app.get('/about', (req, res) => {
@@ -49,9 +72,27 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  await Photo.create(req.body);
-  res.redirect('/');
+  //console.log(req.files.image)
+  //await Photo.create(req.body);
+  //res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  //sync kullandı bunun sebebi klasörün olduğu doğrulanmadan sonraki adıma geçemeyiz
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
