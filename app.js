d295f5d8fd7c1d1c59710a3f07e841a41462930d
Deadburn <cihaom@gmail.com>
2022-05-27 22:06:28 +0300

    deployment

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const pageController = require('./controllers/pageController')
 const app = express();
 
 //connect to DB
-mongoose.connect('mongodb://localhost:27017/pcat-test-db', {
+mongoose.connect('mongodb+srv://cihan:passwd00.@cluster0.uceqxzg.mongodb.net/pcat-db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log("DB CONNECTION SUCCESFULL")
+}).catch((err) => {
+  console.log(err)
 });
 
 // TEMPLATE ENGINE
@@ -42,7 +46,7 @@ app.get('/photos/edit/:id', pageController.getEditPage);
 
 
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
